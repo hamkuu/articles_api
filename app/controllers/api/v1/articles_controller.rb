08d162d247a54f9448dc209f6abb3f86a8959383
Shaw <hamkuu@gmail.com>
2019-10-22 20:04:12 +0900

    :sparkles: create an article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::ArticlesController < ApplicationController
+  def index
+    articles = Article.all.order('created_at ASC')
+
+    render json: articles
+  end
+
+  def create
+    new_article = Article.new(article_params)
+
+    if new_article.save
+      render json: { status: new_article.save, message: new_article }
+    else
+      render json: { status: new_article.save, errors: new_article.errors }
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:author, :title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # api/v1/METHOD_FAMILY.method
   scope module: 'api', path: 'api', as: 'api' do
     scope module: 'v1', path: 'v1', as: 'v1' do
-      resources :articles, only: [:index, :show, :new, :create]
+      get 'articles.list', action: :index, controller: 'articles'
+      post 'article.create', action: :create, controller: 'articles'
+      # resources :articles, only: [:index, :show, :new, :create]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
