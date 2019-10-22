37ee03b987161dabe2c9947c0754229aa2519b55
Shaw <hamkuu@gmail.com>
2019-10-22 21:36:10 +0900

    :sparkles: CRUD operations

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,14 +5,42 @@ class Api::V1::ArticlesController < ApplicationController
     render json: articles
   end
 
+  def show
+    article = Article.find_by(id: params[:id])
+
+    if article.nil?
+      render json: { status: false, message: 'article not found' }
+    else
+      render json: { status: true, article: article }
+    end
+  end
+
   def create
     new_article = Article.new(article_params)
 
-    if new_article.save
-      render json: { status: new_article.save, message: new_article }
+    render json: { status: new_article.save, errors: new_article.errors }
+  end
+
+  def update
+    article = Article.find_by(id: params[:id])
+
+    if article.nil?
+      render json: { status: false, message: 'article not found' }
     else
-      render json: { status: new_article.save, errors: new_article.errors }
+      render json: { status: article.update(article_params) }
     end
+  end
+
+  def destroy
+    article = Article.find_by(id: params[:id])
+
+    if article.nil?
+      render json: { status: false, message: 'article not found' }
+    else
+      article.delete
+    end
+
+    render json: { status: true }
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # api/v1/METHOD_FAMILY.method
   scope module: 'api', path: 'api', as: 'api' do
     scope module: 'v1', path: 'v1', as: 'v1' do
-      get 'articles.list', action: :index, controller: 'articles'
-      post 'article.create', action: :create, controller: 'articles'
-      # resources :articles, only: [:index, :show, :new, :create]
+      resources :articles, only: [:index, :show, :update, :create, :destroy]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
