cb6271a049b54e35c14ecdfec2f7f8952c3e571e
Shaw <hamkuu@gmail.com>
2019-10-23 00:12:33 +0900

    :sparkles: generate users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # api/v1/METHOD_FAMILY.method
   scope module: 'api', path: 'api', as: 'api' do
     scope module: 'v1', path: 'v1', as: 'v1' do
+      resources :users, only: [:index, :show, :new, :create]
       resources :articles, only: [:index, :show, :update, :create, :destroy]
     end
   end
