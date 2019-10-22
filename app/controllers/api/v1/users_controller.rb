92240c3b03ae0bd1af41c5ec8fef7d7adc6349f4
Shaw <hamkuu@gmail.com>
2019-10-23 00:35:01 +0900

    :sparkles: create and list users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,20 @@
 class Api::V1::UsersController < ApplicationController
   def index
+    users = User.all.order('created_at ASC')
+
+    render json: users
   end
 
-  def show
-  end
-
-  def new
-  end
-
+  # POST api/v1/users
   def create
+    new_user = User.new(user_params)
+
+    render json: { status: new_user.save, errors: new_user.errors }
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
