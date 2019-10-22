66beac2c95a759ecffb858287ca882faa7c5418e
Shaw <hamkuu@gmail.com>
2019-10-22 18:48:25 +0900

    :sparkles: basic routes for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # api/v1/METHOD_FAMILY.method
+  scope module: 'api', path: 'api', as: 'api' do
+    scope module: 'v1', path: 'v1', as: 'v1' do
+      resources :articles, only: [:index, :show, :new, :create]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
