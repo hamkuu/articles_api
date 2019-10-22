43abff668590d283eb60fe008702786ece597dd0
Shaw <hamkuu@gmail.com>
2019-10-23 00:53:50 +0900

    :card_file_box: add auth_token to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_143636) do
+ActiveRecord::Schema.define(version: 2019_10_22_155237) do
 
   create_table "articles", force: :cascade do |t|
     t.string "author"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_10_22_143636) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "auth_token"
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
   end
 
 end
